1defb4264267e5960a0dd3d3d216875ac3e4c6fa
AleksNau <workciti@yandex.ru>
2024-05-21 17:02:48 +0300

    category

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import s from "./categoryList.module.scss";
+import Image from "next/image";
+import Link from "next/link";
 
 const CategoryList = () => {
     return (
         <div className={s.container}>
-
+            <h1 className={s.title}></h1>
+            <div className={s.categories}>
+                    <Link href={'/blog?cat=style'} className={`${s.category} ${s.style}`}>
+                        <Image src={'/style.png'} width={32} height={32} className={s.image}/>
+                    </Link>
+            </div>
         </div>
     );
 };
--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import s from "./featured.module.scss";
+import Image from "next/image";
 
 const Featured = () => {
     return (
         <div className={s.container}>
-
+<h1 className={s.title}>
+    <b>Lorem ipsum dolor sit amet </b>
+</h1>
+            <div className={s.post}>
+                <div className={s.imgContainer}>
+                    <Image className={s.image} src={'/p1.jpeg'} alt={'alt'} fill />
+                </div>
+                <div className={s.textContainer}>
+                    <h1 className={s.postTitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias dicta, dignissimos earum error excepturi!</h1>
+                    <p className={s.postDesc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolores ea, eum, id nesciunt non, placeat porro quae quia rem soluta voluptas voluptatem! Culpa dolore eligendi fuga recusandae tempora, tenetur?</p>
+                    <button className={s.postButton}>Read more</button>
+                </div>
+            </div>
         </div>
     );
 };
