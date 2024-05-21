d738cc1fb2f389b2c118dcc8e95fec6cbc4f2f0a
AleksNau <workciti@yandex.ru>
2024-05-21 14:28:17 +0300

    darkmode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,41 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 import NavBar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
+import {ThemeContextProvider} from "@/context/ThemeContext";
 
-const inter = Inter({ subsets: ['latin'] })
+/*import ThemeProvider from "@/providers/ThemeProvider"; использую провайдер2 за место так как не работает*/
+
+import dynamic from 'next/dynamic'
+
+const ThemeContextProvider2 = dynamic(() => import("@/providers/ThemeProvider"), {
+    ssr: false,
+})
+
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
+    title: 'Blog App',
+    description: 'The best blog app!',
 }
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-      <div className={'container'}>
-          <div className="wrapper">
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <ThemeContextProvider>
+            <ThemeContextProvider2>
+                <div className={'container'}>
+                    <div className="wrapper">
 
-              <NavBar/>
-              {children}
-              <Footer/>
-              </div>
-
-      </div>
-      </body>
-    </html>
-  )
+                        <NavBar/>
+                        {children}
+                        <Footer/>
+                    </div>
+                </div>
+            </ThemeContextProvider2>
+        </ThemeContextProvider>
+        </body>
+        </html>
+    )
 }
--- src/components/ThemeToggle/ThemeToggle.js
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -1,12 +1,18 @@
-import React from 'react';
+'use client'
+
+import React, {useContext, useEffect, useState} from 'react';
 import s from "./themeToggle.module.scss";
 import Image from "next/image";
+import {ThemeContext} from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
+    const {theme, toggle} = useContext(ThemeContext)
+console.log(theme)
+
     return (
-        <div className={s.container}>
-<Image src={'/moon.png'} alt={'moon'} width={14} height={14}/>
-<div className={s.ball}></div>
+        <div className={s.container} onClick={toggle} style={theme==='dark'?{backgroundColor:"#ffffff"}:{backgroundColor:"#0f172a"}}>
+            <Image src={'/moon.png'} alt={'moon'} width={14} height={14}/>
+            <div className={`${s.ball} ${theme==='dark'? s.ballleft : s.ballright}`} style={theme==='dark'?{left:1,backgroundColor:"#0f172a"}:{right:1,backgroundColor:"#ffffff"}}></div>
             <Image src={'/sun.png'} alt={'sun'} width={14} height={14}/>
         </div>
     );
