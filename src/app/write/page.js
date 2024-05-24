dbcf990bfacae8fe037c5219d93ca4c70c6eb289
AleksNau <workciti@yandex.ru>
2024-05-24 13:54:52 +0300

    front complete

--- src/app/write/page.js
+++ src/app/write/page.js
@@ -5,31 +5,35 @@ import Image from "next/image";
 import ReactQuill from "react-quill";
 
 const WritePage = () => {
-    const [open,setOpen] = useState(false)
-    const [value,setValue] = useState('')
+    const [open, setOpen] = useState(false)
+    const [value, setValue] = useState('')
 
     return (
         <div className={s.container}>
-            <input type="text" placeholder={'Title'}/>
+            <input type="text" placeholder='Title' className={s.input}/>
             <div className={s.editor}>
                 <button className={s.button} onClick={() => setOpen(!open)}>
                     <Image src={'/plus.png'} className={s.plus} width={16} height={16}/>
                 </button>
 
                 {open &&
-                    (<div className={s.add}>
-                    <button className={s.addButton}>
-                        <Image src={'/image.png'} className={s.plus} width={16} height={16}/>
-                    </button>
-                    <button className={s.addButton}>
-                        <Image src={'/external.png'} className={s.plus} width={16} height={16}/>
-                    </button>
-                    <button className={s.addButton}>
-                        <Image src={'/video.png'} className={s.plus} width={16} height={16}/>
-                    </button>
-                </div>)}
-                <ReactQuill className={s.textArea} theme='bubble' value={value} onChange={setValue} placeHolder={'Tell your story'}></ReactQuill>
+                    (
+                        <div className={s.add}>
+                            <button className={s.addButton} onClick={() => setOpen(!open)}>
+                                <Image src={'/image.png'} alt={'image'} className={s.plus} width={16} height={16}/>
+                            </button>
+                            <button className={s.addButton}>
+                                <Image src={'/external.png'} alt={'image'} className={s.plus} width={16} height={16}/>
+                            </button>
+                            <button className={s.addButton}>
+                                <Image src={'/video.png'} alt={'image'} className={s.plus} width={16} height={16}/>
+                            </button>
+                        </div>)}
+                <ReactQuill className={s.textArea} theme='bubble' value={value} onChange={setValue}
+                            placeHolder={'Tell your story'}/>
             </div>
+                <button className={s.publish}>Publish</button>
+
         </div>
     );
 };
