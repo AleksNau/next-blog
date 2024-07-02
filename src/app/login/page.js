3fa6c879d0cac2f92e6cff980f5ef7472c062e04
AleksNau <workciti@yandex.ru>
2024-07-02 14:29:53 +0300

    try server login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,6 +5,7 @@ import s from "./loginPage.module.scss";
 import {signIn} from "next-auth/react"
 import {useSession} from "next-auth/react";
 import {useRouter} from "next/navigation";
+import {SignIn} from "@/app/utils/SignInButton";
 
 
 
@@ -23,11 +24,17 @@ const LoginPage = () => {
     return (
         <div className={s.container}>
             <div className={s.wrapper}>
-                <div className={s.socialButton} onClick={() => {
+                <form
+                    action={() => {
+                        SignIn()
+                    }}
+                >
+                    <button type="submit">Signin with Google</button>
+                </form>
+                {/*<div className={s.socialButton} onClick={() => {
                     signIn("google")
-
                 }}>Sign in with Google
-                </div>
+                </div>*/}
                 <div className={s.socialButton}>Sign in with Github</div>
                 <div className={s.socialButton}>Sign in with Facebook</div>
             </div>
