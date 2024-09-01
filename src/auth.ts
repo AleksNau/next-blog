4d2d440ca0b3ae91bf9ac2af01f256fcbc118aaf
AleksNau <workciti@yandex.ru>
2024-09-01 14:47:45 +0300

    session fix

--- src/auth.ts
+++ src/auth.ts
@@ -8,4 +8,10 @@ import prisma from "@/lib/prisma";
 export const {handlers, signIn, signOut, auth} = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [Google],
+    callbacks: {
+        session({ session, user }) {
+            session.user.id = user.id
+            return session
+        },
+    }
 })
