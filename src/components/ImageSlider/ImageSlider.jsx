0678b25f1f8df5d55e57be9a6c3fdfd65e0869ce
AleksNau <workciti@yandex.ru>
2024-10-29 17:22:02 +0300

    handmade slider

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,23 +1,37 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext,useRef } from "react";
 import s from "./ImageSlider.module.scss";
 import { testingInfo } from "../../app/data/testData";
 import {MyContext} from "@/context/MyContext";
 
 const ImageSlider = ({data,title}) => {
   const {handleCardClick} = useContext(MyContext);
-
+  const list = useRef(null)
   return (
-    <div className={s.container}>
-    
-        {data?.map((item, index) => {
+    <div className={s.wrapper}>
+    <div className={s.list} ref={list}>
+      <button type={'button'} className={s.before} onClick={()=> {
+       list.current.scrollBy({
+        top: 0,
+        left: 300,
+        behavior: 'smooth'
+      });
+      }}> &#8592;</button>
+      <button type={'button'} className={s.next} onClick={()=> {
+         list.current.scrollBy({
+          top: 0,
+          left: -300,
+          behavior: 'smooth'
+        });
+      }}>&#8594;</button>
+    {data?.map((item, index) => {
           return (
             <div key={index} onClick={()=>handleCardClick(item)}>
-              <img src={item} width={150} alt={`${title}${index}`} />
+              <img className={s.item} src={item} width={250} alt={`${title}${index}`} />
             </div>
           );
         })}
-    
+    </div>
     </div>
   );
 };
